4ace954eab82c5e9dd977e5581ce01809df1e25c
LightY-01 <152372672+LightY-01@users.noreply.github.com>
2024-07-12 11:35:19 +0530

    Add files via upload

---
+++ Game-Hacking/hack.cpp
@@ -0,0 +1,16 @@
+#include <dlfcn.h>
+#include <set>
+#include <map>
+#include <functional>
+#include <string>
+#include <vector>
+#include<iostream>
+#include "libGameLogic.h"
+
+void Player::SetJumpState(bool b){
+    printf("[*] SetJumpState(%d)\n",b);
+}
+
+int main(){
+
+}
